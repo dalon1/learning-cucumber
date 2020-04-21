6af7fa0708aef8f8f5122f71d7cf32ab2ef52244
dalon1 <dannelvision@gmail.com>
2020-04-20 20:16:26 -0400

    Adding gradle build configuration

---
+++ src/app/CucumberMain.java
@@ -0,0 +1,10 @@
+package app;
+
+public class CucumberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Learning a little bit about cucumber");
+	}
+
+}
